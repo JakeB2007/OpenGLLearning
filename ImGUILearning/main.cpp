a354a5ade9bc1194df5d7114799aaf89412571ea
JakeB2007 <Jake_240407@outlook.com>
2025-11-25 21:52:25 +0000

    Initial Commit

---
+++ ImGUILearning/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ImGUILearning
+//
+//  Created by Jake Bignell on 25/11/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
